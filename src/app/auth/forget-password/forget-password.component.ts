8ec9178889830487e599307d72f2ee4e06f333cf
Jhon Molina <desarrollador.frontend@domotes.com>
2025-02-28 11:16:18 -0500

    se actualiza angular a la v19

--- src/app/auth/forget-password/forget-password.component.ts
+++ src/app/auth/forget-password/forget-password.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-forget-password',
-  standalone: true,
-  imports: [],
-  templateUrl: './forget-password.component.html',
-  styleUrl: './forget-password.component.scss',
+    selector: 'app-forget-password',
+    imports: [],
+    templateUrl: './forget-password.component.html',
+    styleUrl: './forget-password.component.scss'
 })
 export default class ForgetPasswordComponent {}
